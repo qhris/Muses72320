58116a8e28da072c7101e1624b620d3d79e1c6d5
Christoffer Hjalmarsson <b.christoffer.hjalmarsson@gmail.com>
2017-06-03 00:41:48 +0200

    remove fine gain control from api, clean up comments

--- src/Muses72320.h
+++ src/Muses72320.h
@@ -26,22 +26,13 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <Arduino.h>
 #include "MusesTypes.h"
 
-/**
- * The Muses72320 IC.
- *
- * Allows communication with the chip over I2C.
- */
 class Muses72320
 {
 public:
-	// contextual data types.
 	using data_t = MusesTypes::data_t;
 	using volume_t = MusesTypes::volume_t;
 	using address_t = MusesTypes::address_t;
 
-	/**
-	 * Creates a new chip which can be controlled on a specific chip address.
-	 */
 	Muses72320(address_t chipAddress);
 
 	/**
@@ -61,26 +52,8 @@ public:
 	 * Input of    0: -0.25 *   0 =    0.00dB [Max volume without gain]
 	 * Input of -105: -0.25 * 105 =  -26.25dB
 	 * Input of -446: -0.25 * 446 = -111.50dB [Minimum volume]
-	 *
-	 * @param left  Left channel volume. Range: [-446, 0].
-	 * @param right Right channel volume. Range: [-446, 0].
 	 */
 	void setVolume(volume_t left, volume_t right);
-
-	/**
-	 * Simple attenuation control with 0.25dB steps utilizing the 0.25dB gain.
-	 *
-	 * Volume control ranges from -111.5dB to 0dB using this function.
-	 * Output dB is calculated as: 0.25dB * volume.
-	 * Input values goes from -446 to 0.
-	 *
-	 * Example:
-	 * Input of    0: -0.25 *   0 =    0.00dB [Max volume without gain]
-	 * Input of -105: -0.25 * 105 =  -26.25dB
-	 * Input of -446: -0.25 * 446 = -111.50dB [Minimum volume]
-	 *
-	 * @param volume Left and right channel volume. Range: [-446, 0].
-	 */
 	void setVolume(volume_t volume) { setVolume(volume, volume); }
 
 	/**
@@ -95,46 +68,12 @@ public:
 	 * Input of -100: 0.5 * -100 =  -50.0dB
 	 * Input of -103: 0.5 * -100 =  -51.5dB
 	 * Input of -223: 0.5 * -223 = -111.5dB [Minumum volume/Max atenuation].
-	 *
-	 * @param left  Left channel attenuation. Range: [-223, 0].
-	 * @param right Right channel attenuation. Range: [-223, 0].
 	 */
 	void setAttenuation(volume_t left, volume_t right);
-
-	/**
-	 * Direct attenuation control of the chip only allowing 0.5dB steps.
-	 *
-	 * Attenuation control ranges from -111.5 to 0dB using this function.
-	 * Output dB is calculated as: 0.5dB * attenuation.
-	 * Input values goes from -223 to 0.
-	 *
-	 * Example:
-	 * Input of    0: 0.5 *    0 =   0.0dB [Max volume/No attenuation].
-	 * Input of -100: 0.5 * -100 =  -50.0dB
-	 * Input of -103: 0.5 * -100 =  -51.5dB
-	 * Input of -223: 0.5 * -223 = -111.5dB [Minumum volume/Max atenuation].
-	 *
-	 * @param left  Left channel attenuation. Range: [-223, 0].
-	 * @param right Right channel attenuation. Range: [-223, 0].
-	 */
 	void setAttenuation(volume_t attenuation)
 		{ setAttenuation(attenuation, attenuation); }
 
 	/**
-	 * Sets the fine gain control bit, adding 0.25dB to the attenuation if set.
-	 *
-	 * Note that this will not work with gain, because setting sets the gain
-	 * to 0.25dB, no matter what value was set before with setGain().
-	 * For use with setAttenuation().
-	 *
-	 * @param enabled Sets gain to 0.25dB if set, or to 0dB if not.
-	 */
-	void setFineGain(bool enabled);
-
-	// gain is constrained to [0, 31.5].
-	// input goes from 0 to 63.
-
-	/**
 	 * Direct gain control of the chip with 0.5dB precision.
 	 *
 	 * Gain can be set in the range 0dB to +31.5dB.
@@ -145,49 +84,20 @@ public:
 	 * Input of  0: 0.5 *  0 =  0.0dB [Minumum gain.]
 	 * Input of 31: 0.5 * 31 = 15.5dB
 	 * Input of 63: 0.5 * 63 = 31.5dB [Maximum gain.]
-	 *
-	 * @param left  Left channel gain control. Range: [0, 63].
-	 * @param right Right channel gain control. Range: [0, 63].
 	 */
 	void setGain(volume_t left, volume_t right);
-
-	/**
-	 * Direct gain control of the chip with 0.5dB precision.
-	 *
-	 * Gain can be set in the range 0dB to +31.5dB.
-	 * Gain is calculated as: 0.5dB * gain.
-	 * Input values goes from 0 to 63.
-	 *
-	 * Example:
-	 * Input of  0: 0.5 *  0 =  0.0dB [Minumum gain.]
-	 * Input of 31: 0.5 * 31 = 15.5dB
-	 * Input of 63: 0.5 * 63 = 31.5dB [Maximum gain.]
-	 *
-	 * @param gain Left and right channel gain control. Range: [0, 63].
-	 */
 	void setGain(volume_t volume) { setGain(volume, volume); }
 
 	/**
 	 * Mutes the left or right channel.
 	 * Call setGain(), setAttenuation() or setVolume() to unmute.
-	 *
-	 * @param left Mutes the left channel if set.
-	 * @param right Mutes the right channel if set.
 	 */
 	void mute(bool left, bool right);
-
-	/**
-	 * Mutes the left and right channel.
-	 * Call setGain(), setAttenuation() or setVolume() to unmute.
-	 */
 	void mute() { mute(true, true); }
 
 	/**
 	 * Enable or disable zero crossing. Defaults to enabled.
-	 *
 	 * Zero crossing only works if the zero crossing terminal pin is set low.
-	 *
-	 * @param enabled Enable or disable zero crossing.
 	 */
 	void setZeroCrossing(bool enabled);
 
@@ -196,10 +106,6 @@ public:
 	 *
 	 * With attenuation link enabled, controlling the left channel attenuation
 	 * also contols the right channel.
-	 * Without attenuation link set the left and right channel are controlled
-	 * independently.
-	 *
-	 * @param enabled Enables or disables attenuation link.
 	 */
 	void setAttenuationLink(bool enabled);
 
@@ -208,10 +114,6 @@ public:
 	 *
 	 * With gain link enabled, controlling the left channel gain
 	 * also contols the right channel.
-	 * Without gain link set the left and right channel are controlled
-	 * independently.
-	 *
-	 * @param enabled Enables or disables gain link.
 	 */
 	void setGainLink(bool enabled);
 
@@ -232,12 +134,6 @@ private:
 
 private:
 	address_t chipAddress;
-
-	// muses state bits:
-	//	 7:     link l/r attenuation
-	//   6:     link l/r gain
-	//   5:     disable zero crossing
-	//   [4-0]: not used
 	data_t states;
 };
 
