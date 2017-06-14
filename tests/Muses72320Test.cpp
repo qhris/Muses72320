f4cce01ff68a379592ccb0c1c345d16dfa14bcb4
Christoffer Hjalmarsson <b.christoffer.hjalmarsson@gmail.com>
2017-06-14 23:29:55 +0200

    add tests for chip select address

--- tests/Muses72320Test.cpp
+++ tests/Muses72320Test.cpp
@@ -3,7 +3,12 @@
 
 #include <catch/catch.hpp>
 
-static byte readNextChipAddress(std::queue<byte>& queue);
+static void clearSPIBytes();
+static byte readSPINextByte();
+static byte readSPINextSelectAddressWithSkip();
+static byte readSPINextSelectAddress();
+static byte readSPINextChipAddress();
+static size_t getSPIByteCount();
 
 TEST_CASE("Correctly sets the default values.", "[Muses72320]")
 {
@@ -36,16 +41,131 @@ TEST_CASE("SPI tranfsers to the correct muses address.", "[Muses72320]")
 		device.begin();
 		device.setAttenuationLeft(0);
 
-		auto& bytes = SPI.getWrittenBytes();
-		REQUIRE(readNextChipAddress(bytes) == addr);
-		REQUIRE(bytes.size() == 0);
+		REQUIRE(getSPIByteCount() == 2);
+		REQUIRE(readSPINextChipAddress() == addr);
 	}
 }
 
-byte readNextChipAddress(std::queue<byte>& queue)
+TEST_CASE("Attenuation is sent on the correct select address.", "[Muses72320]")
 {
+	Muses72320 device(0);
+	device.begin();
+
+	device.setAttenuationLeft(0);
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextSelectAddressWithSkip() == 0);
+
+	device.setAttenuationRight(0);
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextSelectAddressWithSkip() == 0b0010'0000);
+}
+
+TEST_CASE("Gain is sent on the correct select address.", "[Muses72320]")
+{
+	Muses72320 device(0);
+	device.begin();
+
+	device.setGainLeft(0);
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextSelectAddressWithSkip() == 0b0001'0000);
+
+	device.setGainRight(0);
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextSelectAddressWithSkip() == 0b0011'0000);
+}
+
+TEST_CASE("Volume is sent with both attenuation and gain control.", "[Muses72320]")
+{
+	Muses72320 device(0);
+	device.begin();
+
+	device.setVolumeLeft(0);
+	device.setVolumeRight(0);
+
+	bool bAttenuationL = false;
+	bool bAttenuationR = false;
+	bool bGainL = false;
+	bool bGainR = false;
+
+	REQUIRE(getSPIByteCount() == 8);
+
+	for (size_t i = 0; i < 4; ++i) {
+		byte selectAddress = readSPINextSelectAddressWithSkip();
+
+		switch (selectAddress)
+		{
+			case 0b0000'0000: bAttenuationL = true; break;
+			case 0b0001'0000: bGainL = true; break;
+			case 0b0010'0000: bAttenuationR = true; break;
+			case 0b0011'0000: bGainR = true; break;
+			default: break;
+		}
+	}
+
+	REQUIRE(bAttenuationL);
+	REQUIRE(bAttenuationR);
+	REQUIRE(bGainL);
+	REQUIRE(bGainR);
+}
+
+TEST_CASE("State is sent with the correct select address.", "[Muses72320]")
+{
+	Muses72320 device(0);
+	device.begin();
+
+	device.enableAttenuationLink();
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextSelectAddressWithSkip() == 0b0100'0000);
+}
+
+TEST_CASE("Mute data is sent correctly.", "[Muses72320]")
+{
+	Muses72320 device(0);
+	device.begin();
+
+	device.muteLeft();
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextByte() == 0);
+	REQUIRE(readSPINextSelectAddress() == 0b0000'0000);
+
+	device.muteRight();
+	REQUIRE(getSPIByteCount() == 2);
+	REQUIRE(readSPINextByte() == 0);
+	REQUIRE(readSPINextSelectAddress() == 0b0010'0000);
+}
+
+void clearSPIBytes()
+{
+	auto& bytes = SPI.getWrittenBytes();
+	while (!bytes.empty()) bytes.pop();
+}
+
+byte readSPINextByte()
+{
+	auto& queue = SPI.getWrittenBytes();
+	byte value = queue.front();
 	queue.pop();
-	byte address = queue.front();
-	queue.pop();
-	return address & 0b0111;
+	return value;
+}
+
+byte readSPINextSelectAddressWithSkip()
+{
+	readSPINextByte();
+	return readSPINextSelectAddress();
+}
+
+byte readSPINextSelectAddress()
+{
+	return readSPINextByte() & 0b11110000;
+}
+
+byte readSPINextChipAddress()
+{
+	readSPINextByte();
+	return readSPINextByte() & 0b0111;
+}
+
+size_t getSPIByteCount()
+{
+	return SPI.getWrittenBytes().size();
 }
